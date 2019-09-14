0e1f8c6e7003e0267ada8db3f7a89cd307e6968b
Enoch Carter <enoch@dwelo.com>
2019-09-14 11:07:13 -0600

    no message

--- src/lib.rs
+++ src/lib.rs
@@ -40,32 +40,12 @@ fn client_requests(addr: &SocketAddr) -> Box<dyn Future<Item=(), Error=()> + Sen
 
 #[macro_use]
 mod hive_macros;
-mod signal;
-#[derive(Debug, Default)]
-struct Foo{
+pub mod signal;
 
-}
-impl Foo {
-    fn gotValue(&self, v:i32) {
-        println!("got value: {:?}", v)
-    }
-}
 
-//macro_rules! property {
-//    ( $x:expr, i32 ) => {
-//        {
-//            signal::Property{
-//                value: $x,
-//                value_changed: Signal<$t>
-//            }
-//            p
-//        }
-//    };
-//}
 
-fn main() {
-    let mut config = Config::new();
-    config.merge(config::File::with_name("hive.yaml"));
+pub fn run(config: &Config) {
+
     match config.get_int("listen") {
         Ok(port) => {
             let addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), port as u16);
@@ -76,25 +56,5 @@ fn main() {
         }
         _ => println!("No listen port specified, not listening")
     }
-
-    //let mut a = signal::Counter::default();
-
-    let mut p: signal::Property<i32> = Default::default();
-
-
-    p.on_changed.connect(|v|{
-        println!("Inside signal: {:?}", v);
-        sleep(Duration::from_secs(2))
-    });
-
-    p.on_changed.connect(|v|{
-        println!("also Inside signal: {:?}", v);
-    });
-
-    p.set_value(3);
-    p.set_value(4);
-
-    //println!("{}", b.read().expect("failed to get read").value());
-    println!("Done: {:?}", p.value);
 }
 
--- src/signal.rs
+++ src/signal.rs
@@ -40,41 +40,22 @@ impl<T> Signal<T> {
     pub fn connect(&mut self, slot: impl Fn(T) + Send + Sync + 'static) {
         self.slots.write().expect("Failed to get write lock on slots").push(Arc::new(slot));
     }
-
-//    pub fn new() -> Self {
-//        Signal::<T>{
-//            slots: Vec::<Box<dyn Fn(T)>>::new()
-//        }
-//    }
 }
 
 
-//pub trait Property{
-//    fn get() -> PropertyType;
-//    fn set(v:PropertyType);
-//    fn emit();
-//}
 #[derive(Default)]
 pub struct Property<PropertyType>
 {
-    pub value: Option<PropertyType>,
+    value: Option<PropertyType>,
     pub on_changed: Signal<Option<PropertyType>>,
 }
 
-//impl From<i32> for Property<i32> {
-//    fn from(v: i32) -> Self {
-//        Property{
-//            value: Default::default(),
-//            on_changed: Signal::<i32>::new(),
-//        }
-//    }
-//}
 
 impl<PropertyType: Clone> Property<PropertyType> {
-    pub fn set_value(&mut self, v: PropertyType)
+    pub fn set(&mut self, v: PropertyType)
         where PropertyType: std::fmt::Debug + PartialEq + Send + Clone + 'static,
     {
-        println!("<<< Setting Value: {:?}", v);
+
         let v_clone = v.clone();
         let op_v = Some(v);
 
@@ -85,34 +66,9 @@ impl<PropertyType: Clone> Property<PropertyType> {
             println!("do nothing ")
         }
     }
+
+    pub fn get(&self) -> &Option<PropertyType> {
+        &self.value
+    }
 }
 
-
-//#[derive(Default)]
-//pub struct Counter {
-//    pub value: i32,
-//    pub value_changed: Signal<i32>,
-//}
-//
-//impl Counter {
-//    pub fn set_value(&mut self, v: i32) {
-//        if v != self.value {
-//            self.value = v;
-//            self.value_changed.emit(v);
-//        }
-//    }
-//
-//    pub fn value(&self) -> i32 {
-//        self.value
-//    }
-//}
-
-//fn main() {
-//    let mut a = Counter::default();
-//    let b = Arc::new(Mutex::new(Counter::default()));
-//
-//    a.value_changed.connect({let b = b.clone(); move |v| b.lock().set_value(v)});
-//
-//    a.set_value(7);
-//    println!("{}", b.lock().value());
-//}
