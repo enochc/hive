f0a6d6c58f39feb559b94cce4687e8be974711cc
Enoch <mrenoch@gmail.com>
2020-08-08 11:02:06 -0600

    up version

--- examples/just_listen.rs
+++ examples/just_listen.rs
@@ -11,19 +11,26 @@ use failure::_core::time::Duration;
 #[allow(unused_must_use, unused_variables, unused_mut, unused_imports)]
 fn main() {
 
+    // let props_str = r#"
+    // listen = "127.0.0.1:3000"
+    // [Properties]
+    // thingvalue= 1
+    // is_active = true
+    // lightValue = 0
+    // thermostatName = "thermostat"
+    // thermostatTarget_temp = 2
+    // "#;
     let props_str = r#"
     listen = "127.0.0.1:3000"
     [Properties]
-    thingvalue= 1
-    is_active = true
-    lightValue = 0
-    thermostatName = "thermostat"
-    thermostatTarget_temp = 2
+    moveup = false
+    movedown = false
+    speed = 1000
     "#;
     let mut server_hive = Hive::new_from_str("SERVE", props_str);
-    server_hive.get_mut_property("thermostatName").unwrap().on_changed.connect(|value|{
-        println!("<<<< SERV|| THERMOSTAT NAME CHANGED: {:?}", value);
-    });
+    // server_hive.get_mut_property("thermostatName").unwrap().on_changed.connect(|value|{
+    //     println!("<<<< SERV|| THERMOSTAT NAME CHANGED: {:?}", value);
+    // });
     let (mut send_chan, mut receive_chan) = mpsc::unbounded();
     task::spawn(async move {
         server_hive.run().await;
