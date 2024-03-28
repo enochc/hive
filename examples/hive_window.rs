476bf5747160178a5c34e98fec7ab120dca2b891
Enoch Carter <mrenoch@gmail.com>
2024-03-28 15:53:30 -0600

    update window, display basic hive properties

--- examples/hive_window.rs
+++ examples/hive_window.rs
@@ -10,30 +10,8 @@ slint::include_modules!();
 fn main(){
     init_logging(Some(LevelFilter::Debug));
 
-    let mut h = Hive::new("examples/motors.toml");
-    let motors = ["m1","m2","m3","m4"];
-    for m in motors {
-        h.get_mut_property(&Property::hash_id(m)).expect("oops").on_next(move |v|{
-            debug!("{}:: {}",m, v);
-        });
-    }
 
-    //
-    // let property = h.get_mut_property(&Property::hash_id("m1")).expect("sasdf");
-    // let mp = Arc::new(Mutex::new(property.clone()));
-    // let weak_m1 = Arc::downgrade(&mp).clone();
-    // let weak_m1 = h.get_weak_property("m1").clone();
-
-
-    // h.get_mut_property(&Property::hash_id("all")).expect("oops").on_next(move |v| {
-    //     match weak_m1.upgrade() {
-    //         None => {println!("none")}
-    //         Some(w) => {
-    //             (*w.lock().unwrap()).set_value(v);
-    //         }
-    //     }
-    // });
-
+    let mut h = Hive::new("examples/listen_3000.toml");
 
     HiveWindow::launch(Some(h));
 }
--- src/hive_gui.rs
+++ src/hive_gui.rs
@@ -6,13 +6,15 @@ use std::ops::Index;
 
 use crate::hive::Hive;
 use crate::property::{Property};
-
+use std::sync::{Arc, mpsc, RwLock, RwLockReadGuard};
 use futures::{SinkExt, StreamExt};
 use std::rc::Rc;
+use std::time::Duration;
 use log::kv::{Source, Value};
 use toml::macros::IntoDeserializer;
-use crossbeam_channel::{bounded, RecvError, select, Sender};
-use async_std::task::block_on;
+use futures::channel::mpsc::{ channel, Sender};
+
+// use mpsc::{channel, Sender};
 
 pub trait Gui {
     fn launch(hive: Option<Hive>);
@@ -31,44 +33,82 @@ impl HProperty {
 
 slint::include_modules!();
 
-// impl From<&Property> for HProperty {
-//     fn from(value: &Property) -> Self {
-//         HProperty {
-//             name: value.get_name().into(),
-//             value: match value.get_value() {
-//                 None => "__empty".into(),
-//                 Some(v) => v.to_string().into(),
-//             },
-//         }
-//     }
-// }
-
+impl From<&Property> for HProperty {
+    fn from(value: &Property) -> Self {
+        HProperty {
+            name: value.get_name().into(),
+            value: match value.get_value() {
+                None => "__empty".into(),
+                Some(v) => v.to_string().into(),
+            },
+        }
+    }
+}
 impl Gui for HiveWindow {
     fn launch(hive: Option<Hive>) {
-        let (mut sender, mut receiver) = bounded::<Vec<f32>>(1); //channel::<Vec<HProperty>>();
+
+
+        let (mut sender, mut receiver) = channel::<HProperty>(1);
         let window = MainWindow::new().unwrap();
         let window_weak = window.as_weak();
 
-        let mut hive_props: Option<HashMap<u64, Property>> = None;
 
-        let mut props = match hive {
+        let props = match hive {
             None => {
-                vec![0f32, 0f32]
+                vec![HProperty{name:"two".into(),value:"vals".into()}]
             }
-            Some(ref h) => {
-                hive_props = Some(h.properties.clone());
-                h.properties.iter().map(|(p, v)| v.into()).collect()
-            },
+            Some(h) => {
+                h.properties.iter().map(|(p,v)|{
+                    v.into()
+                }).collect()
+
+            }
         };
-        props.sort_by(|a, b| a.name.cmp(&b.name));
         let props_model: Rc<VecModel<HProperty>> = Rc::new(VecModel::from(props));
         let properties_rc = ModelRc::from(props_model);
 
         window.set_properties(properties_rc);
+        let mut sender2 = sender.clone();
 
+        async_std::task::spawn(async move {
 
+            loop {
+                while (!sender2.is_closed()) {
+                    match receiver.next().await {
+                        None => {}
+                        Some(property) => {
+                            println!("<<< property {:?}", property);
 
+                            let handle_copy = window_weak.clone();
+                            slint::invoke_from_event_loop(move || {
+                                let props_model: Rc<VecModel<HProperty>> = Rc::new(VecModel::from(vec![property]));
+                                let properties_rc: ModelRc<HProperty> = ModelRc::from(props_model);
+                                handle_copy.unwrap().set_properties(properties_rc)
+                            }).expect("TODO: panic message");
+                        }
+                    }
+                }
+
+            }
+        });
+
+        window.window().on_close_requested(move||{
+            println!("<< done");
+            return CloseRequestResponse::HideWindow;
+        });
+
+        // async_std::task::spawn(async move {
+        //     let mut x = 0;
+        //     while(x < 5) {
+        //         async_std::task::sleep(Duration::from_secs(3)).await;
+        //         sender.send(HProperty::new("one", &format!("two {}", x))).await.expect("TODO: panic message");
+        //         println!("<<<<<<{}",x);
+        //         x+=1;
+        //     }
+        //
+        // });
 
         window.run().unwrap();
+        println!("<< WTF>>>");
     }
 }
